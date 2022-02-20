5f3465b12d8e36edf3f05de2bb8462e13d2bff76
Nikolay Larionov <nikolay.larionov@wilix.org>
2022-02-20 18:40:07 +0300

    second commit

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
 #!/use/local/bin/env python
+from brain_games.cli import welcome_user
+
 
 def main():
-  print("Welcome to the Brain Games!")
+    print("Welcome to the Brain Games!")
+    welcome_user()
+
 
 if __name__ == "__main__":
-  main()
+    main()
